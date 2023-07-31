20b18b9009174acb8c6d808f52f2ae013d0954b9
Jammin93 <b.ohling@outlook.com>
2023-07-30 20:17:49 -0700

    Updated the patch version from four to five.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ src = Path(__file__).parent
 
 setup(
     name="reata",
-    version="1.1.4",
+    version="1.1.5",
     description=(
         "A simple MySQL DBAPI wrapper for simplifying "
         "data processing pipelines."
